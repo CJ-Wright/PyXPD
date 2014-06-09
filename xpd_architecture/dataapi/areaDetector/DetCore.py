3f7e3ffdc415a82d3a72602b11a944e108100a5d
Christopher J. Wright <cjwright4242@gmail.com>
2014-06-09 11:48:32 -0400

    Added more detector control for gain/offset, going to look at previous work before adding more

--- xpd_architecture/dataapi/areaDetector/DetCore.py
+++ xpd_architecture/dataapi/areaDetector/DetCore.py
@@ -15,6 +15,7 @@ import cothread
 from cothread.catools import *
 from dataapi.config._conf import _conf
 import numpy as np
+import os.path
 
 
 def initDet(_conf):
@@ -27,6 +28,14 @@ def initDet(_conf):
     except:
         raise Exception('Some of the detectors were not found or could not connect\
         , namely:\n %s, pv=%s'% (option,_conf.get('PVs',option)))
+    print 'Finding offset/gain/bad pixel correction files'
+    try:
+        for option in _conf.options('Detector files'):
+            os.path.isfile(option)
+    except:
+        raise Exception('Some of the files were not found or could not connect\
+        , namely:\n %s, pv=%s'% (option,_conf.get('Detector files',option)))
+        
 initDet(_conf)
 
 detectorD=dict()
@@ -66,7 +75,18 @@ def Shutter(state=None):
     elif state in ['close, Close, Cl',0]:
         caput(_conf.get('PVs','shutter'),0)
         Shutter()
+
+
 def Offset(Frames=None):
+    """
+    Reports on the status of the internal offset correction and aquires offset correction
+    
+    Parameters
+    ----------
+    Frames: int
+        Number of frames to aquire for the offset correction
+    
+    """
     if Frames==None:
         if caget(detectorD['pv']+'PEOffsetAvailable')==1:
             print 'Offset Available'
@@ -78,5 +98,34 @@ def Offset(Frames=None):
         caput(detectorD['pv']+'PEAcquireOffset',1)
         while caget(detectorD['pv']+'PECurrentOffsetFrame')!=Frames:
             print 'Collecting frame number '+str(caget(detectorD['pv']+'PECurrentOffsetFrame'))
+        print 'Finished offset aquesition, offset now in use'
         caput(detectorD['pv']+'PEUseOffset',1)
-        
+
+def Gain(Frames=None):
+    """
+    Reports on the status of the internal offset correction and aquires offset correction
+    
+    Parameters
+    ----------
+    Frames: int
+        Number of frames to aquire for the offset correction
+    
+    """
+    if Frames==None:
+        if caget(detectorD['pv']+'PEGainAvailable')==1:
+            print 'Gain Available'
+        else:
+            print 'Offset not yet available'
+    else:
+        caput(detectorD['pv']+'PENumGainFrames',Frames)
+        print 'Starting Gain aquesition'
+        caput(detectorD['pv']+'PEAcquireOffset',1)
+        while caget(detectorD['pv']+'PECurrentGainFrame')!=Frames:
+            print 'Collecting frame number '+str(caget(detectorD['pv']+'PECurrentGainFrame'))
+        print 'Finished Gain aquesition, Gain now in use'
+        caput(detectorD['pv']+'PEUseGain',1)
+
+def load_Offset_and_Gain(Filename=None):
+    if Filename==None:
+        #GUI LOAD FILE PROMPT
+    
